9126888d9de74dac15e99ed22db63c5e92d5499e
yutongdev <17767973668@163.com>
2020-02-25 11:08:23 +0800

    测试

--- src/com/wangyt/Main.java
+++ src/com/wangyt/Main.java
@@ -9,6 +9,6 @@ public class Main {
 	public static void main(String[] args) {
 		int[] a = { 20, 40, 30, 10, 60, 50 };
 		Sort.selectSort(a, a.length);
-		System.out.println(Arrays.toString(a));
+		System.out.println( Arrays.toString(a) );
 	}
 }
