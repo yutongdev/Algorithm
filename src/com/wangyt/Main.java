2b0acc932106c780ef7db999a72fbc9d8cb4447a
yutongdev <17767973668@163.com>
2020-02-25 18:20:20 +0800

    新增希尔排序

--- src/com/wangyt/Main.java
+++ src/com/wangyt/Main.java
@@ -7,9 +7,13 @@ import com.wangyt.sort.Sort;
 public class Main {
 
 	public static void main(String[] args) {
-		int[] a = { 20, 40, 30, 10, 60, 50 };
-		Sort.selectSort(a, a.length);
-		System.out.println( Arrays.toString(a) );
+//		int[] a = { 20, 40, 30, 10, 60, 50 };
+//		int[] a = { 9, 1, 2, 5, 7, 4, 8, 6, 3, 5 };
+//		int[] a = { 1 };
+		int[] a = { 1, 2, 3, 4 };
+//		Sort.selectSort(a, a.length);
+		Sort.shellSort(a);
+		System.out.println(Arrays.toString(a));
 	}
 
 }
--- src/com/wangyt/sort/Sort.java
+++ src/com/wangyt/sort/Sort.java
@@ -6,34 +6,56 @@ public class Sort {
 	 * 选择排序
 	 * @param a 待排序数组
 	 * @param n 数组长度
-	 * @return 返回排序数组
+	 * @return 排好序数组
 	 */
-	public static int[] selectSort(int[] a, int n) {
-		
-		int i;  // 有序区的末尾位置
-		int j;  // 无序区的起始位置
-		int min;  // 无序区中最小元素位置
-		
-		for (i=0; i<n; i++) {
+	public static void selectSort(int[] a, int n) {
+
+		int i; // 有序区的末尾位置
+		int j; // 无序区的起始位置
+		int min; // 无序区中最小元素位置
+
+		for (i = 0; i < n; i++) {
 			min = i;
-			
-			for (j=i+1; j<n; j++) {
+
+			for (j = i + 1; j < n; j++) {
 				if (a[j] < a[min]) {
 					min = j;
 				}
 			}
-			
+
 			if (min != i) {
 				int tmp = a[i];
 				a[i] = a[min];
 				a[min] = tmp;
 			}
 		}
-		
-		return a;
+
 	}
-	
-	public static int[] sort() {
-		return null;
+
+
+	/**
+	 * 希尔排序（插入排序增强版）
+	 * @param a 待排序数组
+	 * @return 排好序数组
+	 */
+	public static void shellSort(int[] a) {
+
+		int gap = a.length / 2;
+
+		while (1 <= gap) {
+
+			for (int i = gap; i < a.length; i++) {
+				int j = 0;
+				int tmp = a[i];
+
+				for (j = i - gap; j >= 0 && tmp < a[j]; j = j - gap) {
+					a[j + gap] = a[j];
+				}
+
+				a[j + gap] = tmp;
+			}
+			gap /= 2;
+		}
+
 	}
 }
